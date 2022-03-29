dc5d71bd2b1d524929df40db418e20416f8d9122
VVKo <v.v.kovdrysh@gmail.com>
2022-03-29 23:01:13 +0300

    data
    * add POST request

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -5,7 +5,7 @@ import  {getDataFromGoogleApp} from "../data/Utils";
 const FeedbackContext = createContext()
 
 // const jsonUrl = 'https://gist.githubusercontent.com/Volodymyr-Kovdrysh/368f472d3de21193171cdce3498939d5/raw/f3af065a83e7a8660a81f1e7f6f960e287bcd855/data.json'
-const googleURL = 'https://script.google.com/macros/s/AKfycbyVK9HuT9pgElv0Lx1fs_W8b9_IG0-38eBRm2T40k2OAbmMagck-f-JiUCT_wYa1dQvvA/exec?action=GET'
+const googleURL = 'https://script.google.com/macros/s/AKfycbyFHN96YvS7zWTKLT_mNVpgiJUYGcLeL9p2MN2VeOO5acjN-mEBjf6lelbf76Z5fF2Uhw/exec'
 
 export const FeedBackProvider = ({children}) => {
 
@@ -16,7 +16,7 @@ export const FeedBackProvider = ({children}) => {
         // getData(jsonUrl).then(data => {
         //     setFeedback(data)
         // })
-        getDataFromGoogleApp(googleURL).then(data => {
+        getDataFromGoogleApp(`${googleURL}?action=GET`).then(data => {
             // console.log('from google', data.feedback)
             setFeedback(data.feedback)
         })
@@ -38,7 +38,13 @@ export const FeedBackProvider = ({children}) => {
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+        // setFeedback([newFeedback, ...feedback])
+
+        getDataFromGoogleApp(`${googleURL}?action=POST&id=${newFeedback.id}&rating=${newFeedback.rating}&text=${newFeedback.text}`).then(data => {
+
+            setFeedback(data.feedback)
+        })
+
     }
 
     const deleteFeedback = (id) => {
