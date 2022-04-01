c4c53fe97b896fdc5da263e3e6ac11e0dca578fb
VVKo <v.v.kovdrysh@gmail.com>
2022-04-01 23:45:55 +0300

    pre tailwind install

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
 
 import FeedbackApp from "./FeedbackApp/FeedbackApp";
+import Spinner from "./FeedbackApp/components/shared/Spinner";
 
 const App = () => {
     return (
         <Router>
             <nav>
 
-                <NavLink to={'/'} >Home </NavLink>
-                <NavLink to={'/feedbackapp'} >Опитувальник </NavLink>
+                <NavLink to={'/'} >Головна </NavLink>
+                <NavLink to={'/feedbackapp'} >Зворотній зв'язок </NavLink>
 
 
             </nav>
 
             <Routes>
+                <Route path='/' element={<Spinner /> } />
                 <Route path='/feedbackapp/*' element={<FeedbackApp /> } />
             </Routes>
 
