b3abab81c0cd7dd21704a6698edbbb661eeeaad4
VVKo <v.v.kovdrysh@gmail.com>
2022-04-02 10:24:42 +0300

    context
    * add PUT
    * fix index.css
    * do some refactoring

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,11 +1,11 @@
 import {v4 as uuidv4} from 'uuid'
 import {createContext, useEffect, useState} from "react";
-import  {getDataFromGoogleApp} from "../data/Utils";
+import  getData from "../data/Utils";
 
 const FeedbackContext = createContext()
 
 // const jsonUrl = 'https://gist.githubusercontent.com/Volodymyr-Kovdrysh/368f472d3de21193171cdce3498939d5/raw/f3af065a83e7a8660a81f1e7f6f960e287bcd855/data.json'
-const googleURL = 'https://script.google.com/macros/s/AKfycbxSDs6g2iirqf4j6sXBmPuy1jg5TRk7xQ-iMvW75NoLCtXZRL0raKLzX71IuGi15NkJ9A/exec'
+const googleURL = 'https://script.google.com/macros/s/AKfycbxqTOww_l7e6jElel2ANi3Mzzpvmjy56fS9PsUzSdZrcqvo-GqVVbRncdAuLGc-5NF4qA/exec'
 
 export const FeedBackProvider = ({children}) => {
 
@@ -13,22 +13,10 @@ export const FeedBackProvider = ({children}) => {
 
     useEffect(()=>{
 
-        // getData(jsonUrl).then(data => {
-        //     setFeedback(data)
-        // })
-        getDataFromGoogleApp(`${googleURL}?action=GET`).then(data => {
-            // console.log('from google', data.feedback)
-            setFeedback(data.feedback)
+
+        getData(`${googleURL}?action=GET`).then(data => {
+            setFeedback(data.feedback.reverse())
         })
-
-        // fetch(jsonUrl).then(response => {
-        //          response.json().then(data => {
-        //             console.log('response data', data)
-        //              setFeedback(data)
-        //         });
-        //
-        //     })
-
     },[])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
@@ -38,11 +26,10 @@ export const FeedBackProvider = ({children}) => {
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
-        // setFeedback([newFeedback, ...feedback])
 
-        getDataFromGoogleApp(`${googleURL}?action=POST&id=${newFeedback.id}&rating=${newFeedback.rating}&text=${newFeedback.text}`).then(data => {
+        getData(`${googleURL}?action=POST&id=${newFeedback.id}&rating=${newFeedback.rating}&text=${newFeedback.text}`).then(data => {
 
-            setFeedback(data.feedback)
+            setFeedback(data.feedback.reverse())
         })
 
     }
@@ -51,9 +38,9 @@ export const FeedBackProvider = ({children}) => {
         if(window.confirm('Ви впевнені, що хочете видалити цей важливий відгук??')
         ){
             // setFeedback(feedback.filter(msg => msg.id !== id))
-            getDataFromGoogleApp(`${googleURL}?action=DELETE&id=${id}`).then(data => {
+            getData(`${googleURL}?action=DELETE&id=${id}`).then(data => {
 
-                setFeedback(data.feedback)
+                setFeedback(data.feedback.reverse())
             })
         }
 
@@ -61,7 +48,11 @@ export const FeedBackProvider = ({children}) => {
 
     // update feedback item
     const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map(item => item.id === id ? {...item, ...updItem} : item))
+
+        getData(`${googleURL}?action=PUT&id=${id}&rating=${updItem.rating}&text=${updItem.text}`).then(data => {
+
+            setFeedback(data.feedback.reverse())
+        })
     }
 
     const editFeedback = (item) => {
