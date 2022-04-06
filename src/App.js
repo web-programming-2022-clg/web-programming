2a35ef18f0132921e755cbe258b927b24dd71fd4
VVKo <v.v.kovdrysh@gmail.com>
2022-04-06 10:47:39 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
 import FeedbackApp from "./FeedbackApp/FeedbackApp";
 import MainPage from "./MainPage/MainPage";
+import {ImEnter, ImExit} from "react-icons/im";
 const App = () => {
     return (
         <Router>
-            <div className="navbar bg-neutral text-neutral-content">
+            <div className="navbar bg-neutral text-neutral-content flex justify-between">
+                <div className="flex justify-start">
                 <NavLink to={'/'} className={({isActive}) => 'btn btn-ghost normal-case text-xl' + (isActive ?' bg-green-500' : '')} >Головна</NavLink>
                 <NavLink to={'/feedbackapp'} className={({isActive}) => 'btn btn-ghost normal-case text-xl' + (isActive ?' bg-green-500' : '')} >Зворотній зв'язок </NavLink>
+                </div>
+                <div className="flex justify-end">
+                    <a className="btn"><ImEnter size={32} /></a>
+                    <a className="ImExit"><ImExit size={32} /></a>
+                </div>
             </div>
 
             <Routes>
