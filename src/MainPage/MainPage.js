6eedb7314979eb2f95cad4a249b3162ad02059b3
VVKo <v.v.kovdrysh@gmail.com>
2022-04-06 10:40:17 +0300

    install sass
    * refactoring

---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Spinner from "../FeedbackApp/components/shared/Spinner";
+import styles from "./MainPage.module.scss"
+import {SiFacebook, SiTwitter, SiYoutube} from "react-icons/si";
+
+const MainPage = () => {
+    return (
+        <div className={styles.parent}>
+            <div>
+                Header
+            </div>
+            <div className={styles.mywindow}>
+                <div className={styles.content}><Spinner/></div>
+            </div>
+            <footer className={styles.basement}>
+                <div className="items-center grid-flow-col">
+                    <p>Copyright © 2022 - All right reserved</p>
+                </div>
+                <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                    <SiTwitter size={32}/>
+                    <SiYoutube size={32}/>
+                    <SiFacebook size={32}/>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default MainPage;
