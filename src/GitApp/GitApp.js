6bf8d0c54919cca0b2b417b8d8b41b502d692b70
VVKo <v.v.kovdrysh@gmail.com>
2022-04-20 10:43:07 +0300

    create UserResults

--- src/GitApp/GitApp.js
+++ src/GitApp/GitApp.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
+import Main from "./components/layout/Main";
 
 const GitApp = () => {
     return (
         <div className="flex flex-col justify-between h-screen">
             <NavBar />
-            GitApp
+            <Main />
             <Footer />
         </div>
     );
---
+++ src/GitApp/components/layout/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserResults from "../users/UserResults";
+
+const Main = () => {
+    return (
+        <div>
+            <UserResults />
+        </div>
+    );
+};
+
+export default Main;
