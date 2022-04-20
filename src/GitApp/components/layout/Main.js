f820c0fc2056cbded1c144669dab7c0e0eddbb41
VVKo <v.v.kovdrysh@gmail.com>
2022-04-20 11:01:14 +0300

    create UserItem

--- src/GitApp/components/layout/Main.js
+++ src/GitApp/components/layout/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Route, Routes} from 'react-router-dom'
 import UserResults from "../users/UserResults";
 
 const Main = () => {
     return (
-        <div>
-            <UserResults />
-        </div>
+        <Routes>
+            <Route path='/' element={<UserResults />} />
+            <Route path='/*' element={<>NotFoundPage</>} />
+        </Routes>
     );
 };
 
